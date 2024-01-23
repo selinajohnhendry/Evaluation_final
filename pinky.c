94f6f8b648838c436738e65a1358ebbe245503de
selinajohnhendry <mail2pinkyjohn@gmail.com>
2024-01-23 15:07:17 +0530

    first commit

---
+++ pinky.c
@@ -0,0 +1,9 @@
+\22BCAE48> git clone https://github.com/selinajohnhendry/Evaluation_final.git
+Cloning into 'Evaluation_final'...
+remote: Enumerating objects: 15, done.
+remote: Counting objects: 100% (5/5), done.
+remote: Compressing objects: 100% (2/2), done.
+remote: Total 15 (delta 3), reused 3 (delta 3), pack-reused 10
+Receiving objects: 100% (15/15), done.
+Resolving deltas: 100% (3/3), done.
+PS D:\22BCAE48>
